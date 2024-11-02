d30105a4e3e0e44e742ff4aedba6214a1ed7d2f8
Arthur Gehlen <1arthurgehlen@gmail.com>
2024-11-02 16:49:47 -0300

    Remoção do Footer, Criação da pasta "js", ajuste na hora de importar o cursor, adição da função "add_task".

--- js/script.js
+++ js/script.js
@@ -2,9 +2,9 @@ const input_task = document.getElementById('input_task');
 const add_task_btn = document.getElementById('input_task_btn');
 const task_area = document.getElementById('task_area');
 
-add_task_btn.addEventListener('click', () => {
+function add_task() {
     const input_task_value = input_task.value;
-
+    
     if (input_task_value != "") {
         let task = document.createElement("div");
         let task_title = document.createElement("p");
@@ -29,35 +29,14 @@ add_task_btn.addEventListener('click', () => {
         alert("Você deve digitar a tarefa para adicionar!");
         return;
     }
+}
+
+add_task_btn.addEventListener('click', () => {
+    add_task();
 })
 
 input_task.addEventListener('keypress', (event) => {
     if (event.keyCode == 13) {
-        const input_task_value = input_task.value;
-
-        if (input_task_value != "") {   
-            let task = document.createElement("div");
-            let task_title = document.createElement("p");
-            task_title.textContent = input_task_value;
-
-            task.classList.add("uncompleted_task");
-            task.appendChild(task_title);
-
-            task_area.appendChild(task);
-
-            input_task.value = "";
-
-            task.addEventListener('click', () => {
-                task.classList.add("completed_task");
-
-                setTimeout(() => {
-                    task.style.display = "none";
-                }, 2000);
-            });
-        }
-        else {
-            alert("Você deve digitar a tarefa para adicionar!");
-            return;
-        }
+        add_task();
     }
 })
